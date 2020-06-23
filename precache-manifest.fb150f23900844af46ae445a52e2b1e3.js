c83d356b40217dd4fd3d5ed75fc0511b2a49f611
Moshe Sommers <msommers@mainsequence.net>
2020-06-23 17:06:39 -0400

    Updates

---
+++ precache-manifest.fb150f23900844af46ae445a52e2b1e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6c56f76c59a1bbadd13c4cd7255d9a",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "f5c9778f5ce74738ebd0",
+    "url": "/snake/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "07b5db4fbe166ade3195",
+    "url": "/snake/static/js/2.102b98a2.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/snake/static/js/2.102b98a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c9778f5ce74738ebd0",
+    "url": "/snake/static/js/main.5201dedf.chunk.js"
+  },
+  {
+    "revision": "ab766b4194d6f1620e7e",
+    "url": "/snake/static/js/runtime-main.29d396f8.js"
+  }
+]);
